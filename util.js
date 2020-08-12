393f7dbdb5313691f3ecda735a0f44a31558f5c8
jjkim <objects76@gmail.com>
2020-08-12 17:37:59 +0900

    with react

---
+++ util.js
@@ -0,0 +1,13 @@
+
+
+export function delegate(target, selector, eventName, handler) {
+    target.addEventListener(eventName, (e) => {
+        for (var target = e.target; target && target != this; target = target.parentNode) {
+            // loop parent nodes from the target to the delegation node
+            if (target.matches(selector)) {
+                handler(e);
+                break;
+            }
+        }
+    });
+}
